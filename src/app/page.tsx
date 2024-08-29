6231037be8cde6cef97422352b97d562ce199f7f
Wes <wal2.designer@gmail.com>
2024-08-29 17:11:15 +0100

    kendo, with app bar menu, and map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,56 @@
-import Image from "next/image";
+"use client"
+import {useState} from "react";
+import {AppBar, AppBarSection, Drawer, DrawerContent, DrawerSelectEvent} from "@progress/kendo-react-layout";
+import {Map, MapLayers, MapTileLayer, TileUrlTemplateArgs} from "@progress/kendo-react-map";
+
 import styles from "./page.module.css";
+import "./theme.scss";
+
+
+const items = [
+    {
+        text: "Avatar",
+        selected: true,
+        route: "/",
+    },
+    {
+        separator: true,
+    },
+    {
+        text: "BottomNavigation",
+        route: "/bottomnavigation",
+    },
+    {
+        separator: true,
+    }
+]
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const [selected, setSelected] = useState(items.findIndex((x) => x.selected === true));
+    const tileUrl = (e: TileUrlTemplateArgs) =>
+        `https://${e.subdomain}.tile.openstreetmap.org/${e.zoom}/${e.x}/${e.y}.png`;
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    return (
+        <main>
+            <AppBar>
+                <AppBarSection>Jaca</AppBarSection>
+            </AppBar>
+            <Drawer expanded={true} mode="push" position={"start"} mini={true} items={items.map((item, index) => ({
+                ...item,
+                selected: index === selected,
+            }))}>
+                <DrawerContent>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                    <Map style={{height:"100vh"}} zoom={15} >
+                        <MapLayers>
+                            <MapTileLayer urlTemplate = {tileUrl}  />
+                        </MapLayers>
+                    </Map>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                </DrawerContent>
+            </Drawer>
+        </main>
+    );
 }
