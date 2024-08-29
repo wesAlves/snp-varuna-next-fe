b65e207e806552ac16cf40069dc7e467588540f1
Wes <wal2.designer@gmail.com>
2024-08-29 20:25:41 +0100

    right pane hide and show

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 "use client"
 import {useState} from "react";
-import {AppBar, AppBarSection, Drawer, DrawerContent, DrawerSelectEvent} from "@progress/kendo-react-layout";
+import {
+    AppBar,
+    AppBarSection,
+    Drawer,
+    DrawerContent,
+    DrawerSelectEvent, PanelBar, PanelBarItem,
+    Splitter,
+    SplitterOnChangeEvent, SplitterPane
+} from "@progress/kendo-react-layout";
 import {Map, MapLayers, MapTileLayer, TileUrlTemplateArgs} from "@progress/kendo-react-map";
 
 import styles from "./page.module.css";
@@ -31,6 +39,22 @@ export default function Home() {
     const tileUrl = (e: TileUrlTemplateArgs) =>
         `https://${e.subdomain}.tile.openstreetmap.org/${e.zoom}/${e.x}/${e.y}.png`;
 
+    // size?: string     min?: string     max?: string     resizable?: boolean     collapsible?: boolean     collapsed?: boolean     scrollable?: boolean     keepMounted?: boolean
+    const [mainPanes, setMainPanes] = useState<Array<any>>([{}, {
+        id:2, collapsible: false, size: "400px", resizable: false, keepMounted: false
+    }])
+
+    const onTogglePane = (event: SplitterOnChangeEvent) => {
+        setMainPanes(event.newState);
+    }
+
+    const changeByClick = () => {
+        const  updatedState = [...mainPanes];
+        updatedState[1].collapsed=!updatedState[1].collapsed;
+        setMainPanes(updatedState);
+
+    }
+
     return (
         <main>
             <AppBar>
@@ -41,14 +65,21 @@ export default function Home() {
                 selected: index === selected,
             }))}>
                 <DrawerContent>
+                    <Splitter panes={mainPanes} onChange={onTogglePane}>
+                        <div>
+                            <button onClick={() => changeByClick()}>Change panel
+                            </button>
 
-                    <Map style={{height:"100vh"}} zoom={15} >
-                        <MapLayers>
-                            <MapTileLayer urlTemplate = {tileUrl}  />
-                        </MapLayers>
-                    </Map>
-
-
+                            <Map style={{height: "100vh"}} zoom={15}>
+                                <MapLayers>
+                                    <MapTileLayer urlTemplate={tileUrl}/>
+                                </MapLayers>
+                            </Map>
+                        </div>
+                        <div>
+                            Right pane
+                        </div>
+                    </Splitter>
                 </DrawerContent>
             </Drawer>
         </main>
